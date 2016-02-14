5f39d9b46b3aca9daee9680baffb25716ddebdc4
Dino Omanovic <d.omanovic@tarent.de>
2016-02-14 20:34:42 +0100

    Finished workshop 3 homework

--- workshop/kvstore/src/kv/kv.go
+++ workshop/kvstore/src/kv/kv.go
@@ -3,25 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"sync"
 )
 
+var mutex =&sync.Mutex{}
 
 func main() {
+	go mainroutine(os.Args, "kvstore.db")
+}
+
+func mainRoutineWithWaitGroup(args []string, waitGroup *sync.WaitGroup, filename string) {
+	waitGroup.Add(1)
+	mainroutine(args, filename)
+	waitGroup.Done()
+}
+func mainroutine(args []string, file string) {
+	mutex.Lock()
 	kvstore:=NewStore()
-	file := "store_simple.db"
 	store, err := kvstore.ReadStore(file)
 	if err != nil {
 		fmt.Printf("error opening store: %v", err.Error())
 		os.Exit(1)
 	}
 
-	if len(os.Args) == 1 {
+	if len(args) == 1 {
 		kvstore.WriteTo(store, os.Stdout)
 		os.Exit(0)
 	}
 
 	shouldSave := false
-	for _, arg := range os.Args[1:] {
+	for _, arg := range args[1:] {
 		if modified := handleCommand(store, arg); modified {
 			shouldSave = modified
 		}
@@ -33,6 +44,8 @@ func main() {
 			os.Exit(2)
 		}
 	}
+	mutex.Unlock()
 }
+
 
 
--- workshop/kvstore/src/kv/store_test.go
+++ workshop/kvstore/src/kv/store_test.go
@@ -6,6 +6,10 @@ import (
 	"io/ioutil"
 	"github.com/stretchr/testify/assert"
 	"fmt"
+	"io"
+	"bytes"
+	"strconv"
+	"sync"
 )
 
 var filename string
@@ -17,12 +21,7 @@ var value1 string
 var value2 string
 
 func TestWriting(t *testing.T) {
-	testmap=map[string]string{
-		key1:value1,
-		key2:value2,
-	}
-
-
+	os.Remove(filename)
 	kvstore.WriteStore(testmap, filename)
 	content,_ := ioutil.ReadFile(filename)
 	contentstr := string(content)
@@ -31,11 +30,39 @@ func TestWriting(t *testing.T) {
 }
 
 func TestReading(t *testing.T) {
+	os.Remove(filename)
 	file, _ := os.Create(filename)
 	fmt.Fprintf(file, "%v=%v\n%v=%v", key1, value1, key2, value2)
 	testmap,_ := kvstore.ReadStore(filename)
 	assert.Equal(t, value1, testmap[key1])
 	assert.Equal(t, value2, testmap[key2])
+}
+
+func TestConcurrency(t *testing.T) {
+	os.Remove(filename)
+	waitGroup := &sync.WaitGroup{}
+	i:=0
+	for i<1000 {
+		go mainRoutineWithWaitGroup([]string{"kv","a"+strconv.Itoa(i)+"=b"+strconv.Itoa(i)}, waitGroup, filename)
+		i++
+	}
+	waitGroup.Wait()
+	file, _ := os.Open(filename)
+	j,_:=lineCounter(file)
+	assert.Equal(t, i, j)
+
+}
+
+func BenchmarkConcurrency(b *testing.B) {
+	b.StopTimer()
+	os.Remove(filename)
+	b.StartTimer()
+
+	for i:=0;i<b.N;i++  {
+		kvstore.WriteStore(testmap, filename)
+		kvstore.ReadStore(filename)
+	}
+
 }
 
 func TestMain(m *testing.M) {
@@ -44,7 +71,32 @@ func TestMain(m *testing.M) {
 	key2="key2"
 	value1="value1"
 	value2="value2"
-	os.Remove(filename)
+	testmap=map[string]string{
+		key1:value1,
+		key2:value2,
+	}
 	kvstore=NewStore()
 	os.Exit(m.Run())
+}
+
+
+func lineCounter(r io.Reader) (int, error) {
+	buf := make([]byte, 8196)
+	count := 0
+	lineSep := []byte{'\n'}
+
+	for {
+		c, err := r.Read(buf)
+		if err != nil && err != io.EOF {
+			return count, err
+		}
+
+		count += bytes.Count(buf[:c], lineSep)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return count, nil
 }
